f49313fd5edd8e14edec85ad33ee45fdf9e62cf9
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2021-12-15 16:50:50 +0000

    Update alpha-labs-companion-plugin.php

--- alpha-labs-companion-plugin.php
+++ alpha-labs-companion-plugin.php
@@ -4,6 +4,7 @@
  * @version 1.0.0
  */
 /*
+GitHub Plugin URI: josephdsouza86/alphalabs-companion-plugin-prototype
 Plugin Name: Alpha Labs companion plugin (prototype)
 Plugin URI: https://alphalabs.net/
 Description: This is a prototype plugin to store common utility code that is often required in projects developed by Alpha Labs. The prototype version is a place where code can be quickly added, but should eventually be promoted to the formal plugin at a later date.
