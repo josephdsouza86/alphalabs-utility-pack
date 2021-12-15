f3ce8b09ec08a6c569709fbadc9fcad6692c6b69
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2021-12-15 16:41:07 +0000

    Add files via upload

---
+++ alpha-labs-companion-plugin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package Alpha Labs Companion Plugin
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Alpha Labs companion plugin (prototype)
+Plugin URI: https://alphalabs.net/
+Description: This is a prototype plugin to store common utility code that is often required in projects developed by Alpha Labs. The prototype version is a place where code can be quickly added, but should eventually be promoted to the formal plugin at a later date.
+Author: Joseph D'Souza
+Version: 1.0.0
+Author URI: https://alphalabs.net/
+*/
+
+/**
+ * As theme development is done outside of the distribution folder we will include the 
+ * source location as a theme path, so that we can develop and debug as usual without
+ * a complex Gulp task syncing theme folders.
+ */
+function use_development_theme_location () {
+
+}
+
+/**
+ * Similar to the above theme function, this would point to the development theme folder
+ * however, it may not work as we would be operating two plugin folders, one populated 
+ * in the distribution folder by composer and then the local development folder. Might
+ * just be best to leave this one for now
+ */
+function use_development_plugins_location () {
+
+}
+
+/**
+ * Make it possible to move the uploads folder out of the webroot so that it cannot be
+ * overwritten or interferred with by the installation. We also will have the added
+ * advantage of being able to reference the live uploads folder from staging copies.
+ * Plus, there is a security benefit to accessing uploaded files via a PHP distribution
+ * end point, rather than directly from the uploads folder.
+ */
+function use_external_uploads_folder () {
+
+}
