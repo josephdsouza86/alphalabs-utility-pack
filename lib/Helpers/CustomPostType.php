07c1a0011c7080af99dc6aec41ef43127e181814
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2024-03-22 12:03:56 +0000

    Added CPT helper commands

---
+++ lib/Helpers/CustomPostType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AlphaLabsUtilityPack\Helpers;
+
+class CustomPostType {
+    private $type;
+    private $options;
+    private $singular;
+    private $plural;
+    private $textdomain;
+
+    public function __construct($type, $options = [], $singular = '', $plural = '', $textdomain = 'alpha-labs-utility') {
+        $this->type = $type;
+        $this->options = $options;
+        $this->singular = !empty($singular) ? $singular : ucwords(str_replace('-', ' ', $this->type));
+        $this->plural = !empty($plural) ? $plural : $this->singular . 's';
+        $this->textdomain = $textdomain;
+
+        if (!isset($this->options['labels'])) {
+            $this->options['labels'] = $this->setDefaultLabels($this->singular, $this->plural, $this->textdomain);
+        }
+
+        add_action('init', array($this, 'registerPostType'));
+    }
+
+    private function setDefaultLabels($singular, $plural, $textdomain) {
+        return array(
+            'name'                  => _x($plural, `{$singular} General Name`, $textdomain),
+            'singular_name'         => _x($singular, `{$singular} Singular Name`, $textdomain),
+            'menu_name'             => __($plural, $textdomain),
+            'name_admin_bar'        => __($singular, $textdomain),
+            'archives'              => __('All ' . $plural, $textdomain),
+            'attributes'            => __($singular . ' Attributes', $textdomain),
+            'parent_item_colon'     => __('Parent ' . $singular . ':', $textdomain),
+            'all_items'             => __('All ' . $plural, $textdomain),
+            'add_new_item'          => __('Add New ' . $singular, $textdomain),
+            'add_new'               => __('Add New', $textdomain),
+            'new_item'              => __('New ' . $singular, $textdomain),
+            'edit_item'             => __('Edit ' . $singular, $textdomain),
+            'update_item'           => __('Update ' . $singular, $textdomain),
+            'view_item'             => __('View ' . $singular, $textdomain),
+            'view_items'            => __('View ' . $plural, $textdomain),
+            'search_items'          => __('Search ' . $plural, $textdomain),
+            'not_found'             => __('No ' . strtolower($plural) . ' found', $textdomain),
+            'not_found_in_trash'    => __('No ' . strtolower($plural) . ' found in Trash', $textdomain),
+            'featured_image'        => __('Featured Image', $textdomain),
+            'set_featured_image'    => __('Set featured image', $textdomain),
+            'remove_featured_image' => __('Remove featured image', $textdomain),
+            'use_featured_image'    => __('Use as featured image', $textdomain),
+            'insert_into_item'      => __('Insert into ' . strtolower($singular), $textdomain),
+            'uploaded_to_this_item' => __('Uploaded to this ' . strtolower($singular), $textdomain),
+            'items_list'            => __($plural . ' list', $textdomain),
+            'items_list_navigation' => __($plural . ' list navigation', $textdomain),
+            'filter_items_list'     => __('Filter ' . strtolower($plural) . ' list', $textdomain),
+        );
+    }
+
+    public function registerPostType() {
+        register_post_type($this->type, $this->options);
+    }
+}
---
+++ lib/Helpers/CustomPostTypeFields.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AlphaLabsUtilityPack\Helpers;
+
+class CustomPostTypeFields {
+    private $postType;
+    private $metaBoxId;
+    private $metaBoxTitle;
+    private $fields;
+
+    public function __construct($postType, $metaBoxId, $metaBoxTitle, $fields) {
+        $this->postType = $postType;
+        $this->metaBoxId = $metaBoxId;
+        $this->metaBoxTitle = $metaBoxTitle;
+        $this->fields = $fields;
+
+        add_action('add_meta_boxes', array($this, 'addMetaBox'));
+        add_action('save_post', array($this, 'saveMetaBoxData'));
+    }
+
+    public function addMetaBox() {
+        add_meta_box(
+            $this->metaBoxId,
+            $this->metaBoxTitle,
+            array($this, 'renderMetaBox'),
+            $this->postType
+        );
+    }
+
+    public function renderMetaBox($post) {
+        wp_nonce_field(basename(__FILE__), $this->metaBoxId . '_nonce');
+        $storedMeta = get_post_meta($post->ID);
+
+        echo '<div><table class="form-table alup-table" role="presentation"><tbody>';
+
+        foreach ($this->fields as $field) {
+            $value = isset($storedMeta[$field['id']]) ? esc_attr($storedMeta[$field['id']][0]) : '';
+            echo '<tr><th scope="row"><label for="' . esc_attr($field['id']) . '">' . esc_html($field['label']) . '</label></th>';
+            echo '<td>' . $this->renderField($field, $value) . '</td></tr>';
+        }
+
+        echo '</tbody></table></div>';
+    }
+
+    private function renderField($field, $value) {
+        if (!isset($field['type'])) {
+            $field['type'] = 'text';
+        }
+        
+        switch ($field['type']) {
+            case 'textarea':
+                return '<textarea name="' . esc_attr($field['id']) . '" id="' . esc_attr($field['id']) . '">' . esc_textarea($value) . '</textarea>';
+    
+            case 'number':
+            case 'datetime':
+                return '<input type="' . esc_attr($field['type']) . '" name="' . esc_attr($field['id']) . '" id="' . esc_attr($field['id']) . '" value="' . esc_attr($value) . '" />';
+    
+            default: // default to text
+                return '<input type="text" name="' . esc_attr($field['id']) . '" id="' . esc_attr($field['id']) . '" value="' . esc_attr($value) . '" />';
+        }
+    }    
+
+    public function saveMetaBoxData($post_id) {
+        if (!isset($_POST[$this->metaBoxId . '_nonce'])) {
+            return;
+        }
+        if (!wp_verify_nonce($_POST[$this->metaBoxId . '_nonce'], basename(__FILE__))) {
+            return;
+        }
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+
+        foreach ($this->fields as $field) {
+            if (isset($_POST[$field['id']])) {
+                update_post_meta($post_id, $field['id'], sanitize_text_field($_POST[$field['id']]));
+            }
+        }
+    }
+}
